f4cef514b4609e15e642a32bce1783e0bb841635
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-05 13:53:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+
+}
+class Student
+{
+
+}
+struct StudentS
+{
+    public void Show()
+    { 
+        Console.WriteLine("this is a student");
+
+    }
+    ~Student()
+    {
+    }
+}
